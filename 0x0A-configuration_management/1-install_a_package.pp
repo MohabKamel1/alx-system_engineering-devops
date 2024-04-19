bdccf5d18ffe9910b170c541357780f99974983a
Mohabkamel1 <mohab@Mohab>
2024-04-19 16:04:28 +0200

    1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# Using puppet to install flask from pip3
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Installs puppet-lint, version 2.1.1
+
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
